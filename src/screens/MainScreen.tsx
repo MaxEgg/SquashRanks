0117e9a18c0339da10b748d4a4d7c61aec2fb09c
Max <max_eggerding@hotmail.com>
2019-08-07 20:45:41 +0200

    init

---
+++ src/screens/MainScreen.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+interface IMainScreen {
+}
+
+interface IMainScreenState {
+    isFontLoaded: boolean;
+}
+
+interface IMainScreenState {
+    isFontLoaded: boolean;
+}
+class MainScreen extends React.PureComponent<IMainScreen, IMainScreenState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            isFontLoaded: false
+        };
+    }
+
+    public render(){
+      return (
+        <View>
+            <Text>Inside the app</Text>
+        </View>
+      )
+    };
+}
+
+export default MainScreen;
